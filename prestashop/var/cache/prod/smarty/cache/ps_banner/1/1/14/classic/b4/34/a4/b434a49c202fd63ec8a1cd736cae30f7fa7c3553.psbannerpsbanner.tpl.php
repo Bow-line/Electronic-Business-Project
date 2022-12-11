814713532a0e27164769c53c689c9da119ee21d7
root <root@LAPTOP-VANDDIM5.localdomain>
2022-12-11 16:22:44 +0100

    Change localization settings.

---
+++ prestashop/var/cache/prod/smarty/cache/ps_banner/1/1/14/classic/b4/34/a4/b434a49c202fd63ec8a1cd736cae30f7fa7c3553.psbannerpsbanner.tpl.php
@@ -0,0 +1,26 @@
+<?php
+/* Smarty version 3.1.43, created on 2022-12-11 16:00:53
+  from 'module:psbannerpsbanner.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.43',
+  'unifunc' => 'content_6395f0a5813f18_39774497',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '64034093fbda864710a2c0f526b37f921e53ee39' => 
+    array (
+      0 => 'module:psbannerpsbanner.tpl',
+      1 => 1670682609,
+      2 => 'module',
+    ),
+  ),
+  'cache_lifetime' => 31536000,
+),true)) {
+function content_6395f0a5813f18_39774497 (Smarty_Internal_Template $_smarty_tpl) {
+?><a class="banner" href="http://localhost:8080/index.php?id_category=16&amp;controller=category&amp;id_lang=2" title="">
+      <img src="http://localhost:8080/modules/ps_banner/img/ecaa0d1d72d76c5b11df4a4afc4f7be6.jpg" alt="" title="" class="img-fluid" loading="lazy" width="1110" height="213">
+  </a>
+<?php }
+}
---
+++ prestashop/var/cache/prod/smarty/cache/ps_imageslider/1/1/1/14/classic/e6/7f/55/e67f55521aff59354af18d404492946206124e65.psimagesliderviewstemplat.php
@@ -0,0 +1,84 @@
+<?php
+/* Smarty version 3.1.43, created on 2022-12-11 14:23:54
+  from 'module:psimagesliderviewstemplat' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.43',
+  'unifunc' => 'content_6395d9ea3afa05_24384599',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '6c2108a17c7103b6e203f4f0621d4645b56b0114' => 
+    array (
+      0 => 'module:psimagesliderviewstemplat',
+      1 => 1670682609,
+      2 => 'module',
+    ),
+  ),
+  'cache_lifetime' => 31536000,
+),true)) {
+function content_6395d9ea3afa05_24384599 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+  <div id="carousel" data-ride="carousel" class="carousel slide" data-interval="5000" data-wrap="true" data-pause="hover" data-touch="true">
+    <ol class="carousel-indicators">
+            <li data-target="#carousel" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel" data-slide-to="1"></li>
+            <li data-target="#carousel" data-slide-to="2"></li>
+          </ol>
+    <ul class="carousel-inner" role="listbox" aria-label="Carousel container">
+              <li class="carousel-item active" role="option" aria-hidden="false">
+          <a href="http://localhost:8080/index.php?id_category=23&amp;controller=category&amp;id_lang=2">
+            <figure>
+              <img src="http://localhost:8080/modules/ps_imageslider/images/18944dc07bb4ca3041edab6ad1e323c6e15f2d0a_8b0b84c697990f28292dfc1789a009d7.jpg" alt="sample-1" loading="lazy" width="1110" height="340">
+                              <figcaption class="caption">
+                  <h2 class="display-1 text-uppercase">Akcesoria dla lalek</h2>
+                  <div class="caption-description"><h3>Dodatki dla Twojej lalki</h3>
+<p class="MsoNormal"><span style="font-size:8.5pt;line-height:107%;font-family:Verdana, sans-serif;color:#000000;background:#FAFBFC;">W naszej ofercie mamy przeróżne akcesoria dla lalek. Znajdziesz tu ubranka, domki oraz wiele innych! Kliknij i sprawdź sam :)</span></p></div>
+                </figcaption>
+                          </figure>
+          </a>
+        </li>
+              <li class="carousel-item " role="option" aria-hidden="true">
+          <a href="http://localhost:8080/index.php?id_category=24&amp;controller=category&amp;id_lang=2">
+            <figure>
+              <img src="http://localhost:8080/modules/ps_imageslider/images/c8795112d3d15080f54020e68f364ae2c17ade6e_barbieittakestwosdp_background_na_01_en-1280.jpg" alt="sample-2" loading="lazy" width="1110" height="340">
+                              <figcaption class="caption">
+                  <h2 class="display-1 text-uppercase">Księżniczki i wróżki</h2>
+                  <div class="caption-description"><p class="MsoNormal" style="line-height:normal;"><span style="color:#ffffff;"><b><span style="font-size:13.5pt;font-family:Verdana, sans-serif;">Lubisz księżniczki i wróżki?</span></b></span></p>
+<p style="font-size:11px;font-weight:400;"><span style="color:#ffffff;"><b></b></span></p>
+<p style="font-size:11px;font-weight:400;"><span style="color:#ffffff;"><b></b></span></p>
+<p class="MsoNormal" style="line-height:normal;"><span style="font-size:8.5pt;font-family:Verdana, sans-serif;color:#ffffff;">Świetnie się składa! W naszej ofercie jest każda księżniczka jaka przyjdzie Ci na myśl. Sprawdź sam :)</span></p></div>
+                </figcaption>
+                          </figure>
+          </a>
+        </li>
+              <li class="carousel-item " role="option" aria-hidden="true">
+          <a href="http://localhost:8080/index.php?id_category=17&amp;controller=category&amp;id_lang=2">
+            <figure>
+              <img src="http://localhost:8080/modules/ps_imageslider/images/ef77a766673f9d6d4b98821b312eba186ba3ad67_maxresdefault (1).jpg" alt="sample-3" loading="lazy" width="1110" height="340">
+                              <figcaption class="caption">
+                  <h2 class="display-1 text-uppercase">Zwierzaki i minilalki</h2>
+                  <div class="caption-description"><h3><span style="font-family:Verdana, sans-serif;color:#ffffff;">Marzysz o zwierzaku dla swojej lalki?</span></h3>
+<p></p>
+<p><span style="font-size:8.5pt;font-family:Verdana, sans-serif;color:#ffffff;">W tej sekcji znajdziesz kompana dla swojej lalki i nie tylko! :)</span></p></div>
+                </figcaption>
+                          </figure>
+          </a>
+        </li>
+          </ul>
+    <div class="direction" aria-label="Carousel buttons">
+      <a class="left carousel-control" href="#carousel" role="button" data-slide="prev" aria-label="Previous">
+        <span class="icon-prev hidden-xs" aria-hidden="true">
+          <i class="material-icons">&#xE5CB;</i>
+        </span>
+      </a>
+      <a class="right carousel-control" href="#carousel" role="button" data-slide="next" aria-label="Next">
+        <span class="icon-next" aria-hidden="true">
+          <i class="material-icons">&#xE5CC;</i>
+        </span>
+      </a>
+    </div>
+  </div>
+<?php }
+}
