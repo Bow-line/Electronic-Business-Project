db4884353e43658d186a9040b9c9c179c9277ba7
IllBeWatchingU <101195052+IllBeWatchingU@users.noreply.github.com>
2022-12-12 16:53:26 +0100

    Update selenium tests

--- selenium_tests/test.py
+++ selenium_tests/test.py
@@ -2,7 +2,6 @@ import random
 import string
 from time import sleep
 from selenium import webdriver
-from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 
 
@@ -10,18 +9,16 @@ def get_random_string(letters):
     ret = ''.join(random.sample(string.ascii_lowercase, letters))
     return ret
 
+
 if __name__ == "__main__":
-    driver = webdriver.Edge()
+    driver = webdriver.Firefox()
     driver.maximize_window()
     driver.get("http://localhost:80")
-    driver.find_element(By.ID, "details-button").click()
-    driver.find_element(By.ID, "proceed-link").click()
-    sleep(1)
-
+    sleep(5)
     # stwórz konto
     driver.find_element(By.CLASS_NAME, "user-info").click()
     driver.find_element(By.LINK_TEXT, "Nie masz konta? Załóż je tutaj").click()
-    sleep(1)
+    sleep(2)
     driver.find_element(By.ID, "field-id_gender-" + str(random.randint(1, 2))).click()
     driver.find_element(By.ID, "field-firstname").send_keys(get_random_string(random.randint(4, 10)))
     driver.find_element(By.ID, "field-lastname").send_keys(get_random_string(random.randint(4, 10)))
@@ -30,7 +27,7 @@ if __name__ == "__main__":
     driver.find_element(By.NAME, "customer_privacy").click()
     driver.find_element(By.NAME, "psgdpr").click()
     driver.find_element(By.CLASS_NAME, "form-control-submit").click()
-    sleep(1)
+    sleep(2)
 
     # załadowanie pierwszej kategorii
     driver.find_element(By.ID, 'category-3').click()
@@ -69,16 +66,16 @@ if __name__ == "__main__":
     # usunięcie elementu z koszyka
     driver.find_element(By.ID, "_desktop_cart").click()
     driver.find_element(By.CLASS_NAME, "remove-from-cart").click()
-    sleep(1)
+    sleep(2)
 
     # wypełnienie adresu zamówienia
     driver.find_element(By.CLASS_NAME, "cart-detailed-actions").click()
     driver.find_element(By.ID, "field-address1").send_keys(get_random_string(random.randint(8, 16)) + str(random.randint(1, 15)))
     driver.find_element(By.ID, "field-postcode").send_keys(str(random.randint(10, 99)) + '-' + str(random.randint(100, 999)))
     driver.find_element(By.ID, "field-city").send_keys(get_random_string(random.randint(8, 16)))
-    sleep(1)
+    sleep(2)
     driver.find_element(By.NAME, "confirm-addresses").click()
-    sleep(1)
+    sleep(2)
 
     # wybór sposobu dostawy
     driver.find_element(By.ID, "delivery_option_9").click()
